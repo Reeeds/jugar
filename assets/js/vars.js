3882e72a55120de2d8fd3722bdad7dbaada0eab8
Reeeds <reto.schuermann@gmail.com>
2017-01-20 23:10:00 +0100

    navigation with jquery, no url change

---
+++ assets/js/vars.js
@@ -0,0 +1,8 @@
+var currenttab=null;
+
+var sites = [
+	{id:"site1",title:"Seite 1", text: "Blablablajasdfkasdf"},
+	{id:"site2",title:"Seite 2", text: "223452345234523452345"},
+	{id:"site3",title:"Seite 3", text: "jkljklkjljkljkljkljlj"},
+	{id:"site4",title:"Seite 4", text: "yxyxyxyxyxyxyxyxyxyxyx"}
+];
